52f4f62d12004dd713932dc1e3622d4eb4fec627
Yoshiki Komachi <yosh1k104@dhcp176.ht.sfc.keio.ac.jp>
2014-12-04 07:28:47 +0900

    modified tex_compile.sh not to copy backup file

--- tex_compile.sh
+++ tex_compile.sh
@@ -22,21 +22,5 @@ rm ${AUXFILE}.log
 rm ${AUXFILE}.lot
 rm ${AUXFILE}.toc
 
-
-NUMBER=1
-
-for i in `ls ../old`
-do
-    if [ $NUMBER -eq 1 ]; then
-        DELETE_FILE=${i}
-    fi
-done
-
-if [ $NUMBER -gt 15 ]; then
-    rm -r ../old/$DELETE_FILE
-fi
-
-cp -r ../thesis ../old/thesis_`date +%m`:`date +%d`_`date +%H`:`date +%M`:`date +%S`
-
 #rsync -avz --delete . romen@dali.ht.sfc.keio.ac.jp:/home/romen/graduation_thesis/
 #rsync -avz --delete . ysk@dali.ht.sfc.keio.ac.jp:/home/ysk/
